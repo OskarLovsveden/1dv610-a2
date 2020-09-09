2dff2b4afd8f0de97977bd17549860c5b6328ee7
ol222hf <ol222hf@student.lnu.se>
2020-09-09 21:18:00 +0200

    added registerView on LayoutView

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -1,27 +1,40 @@
 <?php
 
-
 class LayoutView {
-  
-  public function render($isLoggedIn, LoginView $v, DateTimeView $dtv) {
-    echo '<!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Login Example</title>
-        </head>
-        <body>
-          <h1>Assignment 2</h1>
-          ' . $this->renderIsLoggedIn($isLoggedIn) . '
-          
-          <div class="container">
-              ' . $v->response() . '
-              
-              ' . $dtv->show() . '
-          </div>
-         </body>
-      </html>
-    ';
+
+  public function render($isLoggedIn, LoginView $loginView, RegisterView $registerView, DateTimeView $dtv) {
+    $renderHTML = '
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <title>Login Example</title>
+    </head>
+    <body>
+    <h1>Assignment 2</h1>';
+
+    if (isset($_GET["register"])) {
+      $renderHTML .= '<a href="?">Back to login</a>
+      ' . $this->renderIsLoggedIn($isLoggedIn) . '
+
+      <div class="container">
+      <h2>Register new user</h2>
+      ' . $registerView->response() . '
+      ' . $dtv->show() . '
+      </div>';
+    } else {
+      $renderHTML .= '<a href="?register">Register a new user</a>
+      ' . $this->renderIsLoggedIn($isLoggedIn) . '
+
+      <div class="container">
+      ' . $loginView->response() . '
+      ' . $dtv->show() . '
+      </div>';
+    }
+
+    $renderHTML .= '</body></html>';
+
+    echo $renderHTML;
   }
   
   private function renderIsLoggedIn($isLoggedIn) {
