a0515192e2536effd478e1656a77c6626a85110b
ol222hf <ol222hf@student.lnu.se>
2020-09-09 21:17:28 +0200

    require registerView

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 //INCLUDE THE FILES NEEDED...
 require_once('view/LoginView.php');
+require_once('view/RegisterView.php');
 require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
 require_once('Database.php');
@@ -12,16 +13,20 @@ $password = "users";
 $dbName = "users";
 
 $db = new Database($servername, $username, $password, $dbName);
-$db->tryConnection();
+$connectionString = $db->tryConnection();
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 //CREATE OBJECTS OF THE VIEWS
-$v = new LoginView();
+$loginView = new LoginView();
+$registerView = new RegisterView();
 $dtv = new DateTimeView();
 $lv = new LayoutView();
 
+$lv->render(false, $loginView, $registerView, $dtv);
 
-$lv->render(false, $v, $dtv);
+if (mb_strlen($connectionString) > 0) {
+    // echo $connectionString;
+}
