e80acc8c42d896b392db772f7e167253c6450584
OskarLovsveden <ol222hf@student.lnu.se>
2020-09-10 17:19:06 +0200

    adding password model

---
+++ model/Password.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Model;
+
+class Password {
+    private $password;
+
+    public function __construct(string $password) {
+        $this->password = $password;
+    }
+
+    public function getPassword() : string {
+        return $this->password;
+    }
+}
