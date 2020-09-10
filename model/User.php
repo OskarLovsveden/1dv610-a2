792a070f876ea5bb9e02c7fba59a94f38491de30
OskarLovsveden <ol222hf@student.lnu.se>
2020-09-10 17:19:37 +0200

    modified user model

--- model/User.php
+++ model/User.php
@@ -4,4 +4,25 @@ namespace Model;
 
 class User {
 
+    private $username;
+    private $password;
+    private $userID;
+
+    public function __construct(Username $username, Password $password, UserID $userID) {
+        $this->username = $username;
+        $this->password = $password;
+        $this->userID = $userID;
+    }
+
+    public function getUsername() : Username {
+        return $this->username;
+    }
+
+    public function getPassword() : Password {
+        return $this->password;
+    }
+
+    public function getUserID() : UserID {
+        return $this->userID;
+    }
 }
