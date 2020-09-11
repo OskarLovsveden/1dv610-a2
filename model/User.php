8aa48095e01dfc7a3133aa0ab68a7246ff9c4f11
OskarLovsveden <ol222hf@student.lnu.se>
2020-09-11 16:44:43 +0200

    fix constructor

--- model/User.php
+++ model/User.php
@@ -7,7 +7,7 @@ class User {
     private $username;
     private $password;
 
-    public function __construct(Username $username, Password $password) {
+    public function __construct(\Model\Username $username, \Model\Password $password) {
         $this->username = $username;
         $this->password = $password;
     }
