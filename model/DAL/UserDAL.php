573d8c46692f3dda9db2e923e5244e26be3e1a23
OskarLovsveden <ol222hf@student.lnu.se>
2020-09-22 11:45:30 +0200

    adding empty registerUser method

--- model/DAL/UserDAL.php
+++ model/DAL/UserDAL.php
@@ -3,11 +3,16 @@
 namespace Model\DAL;
 
 class UserDAL {
+
+    public function registerUser(\model\User $user) {
+        // TODO register user
+    }
+
     public static function findUserByName(\Model\Credentials $credentials) : \Model\User {
         $username = $credentials->getUsername();
         $password = $credentials->getPassword();
         
-        // TEMP CODE "User exists"
+        // TODO: fix TEMP CODE "User exists" once registration is in place
         if ($username == "Admin" && password_verify("Password", $password)) {
             $testUserName = new \Model\Username($username);
             $testPassword = new \Model\Password($password);
@@ -15,8 +20,8 @@ class UserDAL {
             $testUser = new \Model\User($testUserName, $testPassword);
             return $testUser;
         }
+        // end
 
-        throw new \Exception("User does not exist.");
-        // throw new \Exception("Wrong name or password");
+        throw new \Exception("Wrong name or password");
     }
 }
